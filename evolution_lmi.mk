acb899ed8cbd61404ec1ff99b2e809e637df735d
martinjesusdev <martin@virtualsw.com>
2021-07-17 11:52:14 +0200

    lmi: Init Evo X based on Android 11

--- evolution_lmi.mk
+++ evolution_lmi.mk
@@ -8,19 +8,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Derp stuff
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-# Derp specific properties
-PRODUCT_PRODUCT_PROPERTIES += \
-  ro.derp.maintainer=Schnedi \
-  ro.derp.cpu=sm8250 \
-  ro.derp.rom=Derp
+# Inherit some common Evolution X stuff
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+EVO_BUILD_TYPE := OFFICIAL
+EXTRA_FOD_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := derp_lmi
+PRODUCT_NAME := evolution_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
